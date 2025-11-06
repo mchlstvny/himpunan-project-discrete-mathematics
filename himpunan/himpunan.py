7443a3a0a6893f95a1bb954d76f5ec6640ad516b
Michele <micheledatii@gmail.com>
2025-11-06 14:06:51 +0800

    init

--- himpunan/himpunan.py
+++ himpunan/himpunan.py
@@ -1,57 +1,56 @@
+from decimal import Decimal
+from fractions import Fraction
+
 class Himpunan:
     def __init__(self, *args):
         self.data = []
         for item in args:
             if isinstance(item, (list, set, tuple)):
                 for i in item:
-                    if i not in self.data:
-                        self.data.append(i)
+                    self._tambah(i)
             else:
-                if item not in self.data:
-                    self.data.append(item)
+                self._tambah(item)
+
+    def _tambah(self, item):
+        val = float(item) if isinstance(item, (int, float, Decimal, Fraction)) else item
+        if val not in self.data:
+            self.data.append(val)
 
     def __repr__(self):
         return "{" + ", ".join(map(str, self.data)) + "}"
 
-    # Tambah anggota
     def __iadd__(self, item):
-        if item not in self.data:
-            self.data.append(item)
+        self._tambah(item)
         return self
 
-    # Hapus anggota
     def __isub__(self, item):
-        if item in self.data:
-            self.data.remove(item)
+        val = float(item) if isinstance(item, (int, float, Decimal, Fraction)) else item
+        if val in self.data:
+            self.data.remove(val)
         return self
 
-    # Panjang himpunan
     def __len__(self):
         return len(self.data)
 
-    # Cek apakah item ada dalam himpunan
     def __contains__(self, item):
-        return item in self.data
+        val = float(item) if isinstance(item, (int, float, Decimal, Fraction)) else item
+        return val in self.data
 
-    # Cek kesetaraan
     def __eq__(self, other):
         return sorted(self.data) == sorted(other.data)
 
-    # Subset, proper subset, dan superset
     def __le__(self, other):
-        return all(item in other.data for item in self.data)
+        return all(x in other.data for x in self.data)
 
     def __lt__(self, other):
         return self <= other and self != other
 
     def __ge__(self, other):
-        return all(item in self.data for item in other.data)
+        return all(x in self.data for x in other.data)
 
-    # Ekuivalen (elemen sama, urutan bebas)
     def __floordiv__(self, other):
         return set(self.data) == set(other.data)
 
-    # Gabungan (A ∪ B)
     def __add__(self, other):
         result = list(self.data)
         for x in other.data:
@@ -59,28 +58,23 @@ class Himpunan:
                 result.append(x)
         return Himpunan(*result)
 
-    # Irisan (A ∩ B)
     def __truediv__(self, other):
         result = [x for x in self.data if x in other.data]
         return Himpunan(*result)
 
-    # Selisih (A - B)
     def __sub__(self, other):
         result = [x for x in self.data if x not in other.data]
         return Himpunan(*result)
 
-    # Selisih Simetris (A ⊕ B)
     def __mul__(self, other):
         result = [x for x in self.data if x not in other.data] + \
                  [x for x in other.data if x not in self.data]
         return Himpunan(*result)
 
-    # Perkalian Kartesius (A × B)
     def __pow__(self, other):
         result = [(a, b) for a in self.data for b in other.data]
         return result
 
-    # Himpunan Kuasa
     def ListKuasa(self):
         hasil = [[]]
         for elemen in self.data:
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='himpunan-michele',
+    name='himpunan-group1-ucmakassar',
     version='0.1.0',
     packages=find_packages(),
-    author='Michele Stevany Venda Dati',
-    author_email='your_email@example.com',
+    author='Kelompok 2',
+    author_email='mchlstvny@gmail.com',
     description='Implementasi konsep himpunan dengan Python',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
